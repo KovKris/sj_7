67f9a0b260e3a6d827cbdfb160cd0c2d37419715
KovKris <kovacs.kristof123@gmail.com>
2025-02-25 13:52:59 +0100

    Prvá verzia

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hlavný nadpis</h1>
+    <?php
+        echo "<h2>Nadpis 2</h2>";
+        
+        $x = 10;
+        echo $x;
+        
+        $meno = 'Peter';
+        echo $meno;
+        echo '<br>';
+        var_dump($meno);
+        
+        $farby = array("cervena","modra","zelena");
+        echo $farby[0];
+        echo("<br>");
+        
+        foreach($farby as $farba){
+            echo "Farba:". $farba. "<br>";
+        }
+        $data = array("Peter" => 25,
+                "Jana" => 30,
+                "Marek" => 20
+            );
+        foreach($data as $name => $age){
+            echo "$name ma $age rokov <br>";
+        }
+    ?>
+</body>
+</html>
